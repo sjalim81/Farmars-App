71ccf0986934f420038b896ca12b0e7c20e5aeb0
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2020-05-07 23:17:23 +0600

    search bar bug fixed

--- app/src/main/java/com/example/farmersapp/MarketFragment.java
+++ app/src/main/java/com/example/farmersapp/MarketFragment.java
@@ -1,12 +1,15 @@
 package com.example.farmersapp;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -15,56 +18,42 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MarketFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MarketFragment extends Fragment {
-  // TODO: Rename parameter arguments, choose names that match
-  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
   private static final String ARG_PARAM1 = "param1";
   private static final String ARG_PARAM2 = "param2";
   private FloatingActionButton floatingAddNewItemButton;
 
-  // TODO: Rename and change types of parameters
   private String mParam1;
   private String mParam2;
 
   RecyclerView marketRecyleView;
   ListAdapter_Market marketListAdapter;
-  ArrayList<CustomListItem_Market> mData;
+  ArrayList<productsListOfMarketFirestore> mData;
   ConstraintLayout rootLayout;
   EditText searchInput;
   CharSequence search = "";
   FirestoreRecyclerOptions<productsListOfMarketFirestore> options;
 
+  AlternateListAdapter_Market adapter;
+
   private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
   private CollectionReference productsOfMarketCollectionRef = firebaseFirestore.collection("products_of_market");
 
-
-
-
   public MarketFragment() {
-    // Required empty public constructor
   }
 
-  /**
-   * Use this factory method to create a new instance of
-   * this fragment using the provided parameters.
-   *
-   * @param param1 Parameter 1.
-   * @param param2 Parameter 2.
-   * @return A new instance of fragment MarketFragment.
-   */
-  // TODO: Rename and change types and number of parameters
   public static MarketFragment newInstance(String param1, String param2) {
     MarketFragment fragment = new MarketFragment();
     Bundle args = new Bundle();
@@ -83,7 +72,7 @@ public class MarketFragment extends Fragment {
     }
   }
   productsListOfMarketFirestore[] item = new productsListOfMarketFirestore[100];
-  int indx =0;
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
@@ -97,41 +86,34 @@ public class MarketFragment extends Fragment {
     mData = new ArrayList<>();
 
 
-//    searchInput.setBackgroundResource(R.drawable.search_input_style);
+    searchInput.setBackgroundResource(R.drawable.search_input_style);
     rootLayout.setBackgroundColor(getResources().getColor(R.color.white));
 
+      setUpRecyclerViewManual();
+   getDataToArray();
 
-    setUpRecyclerView();
-   mData =  marketListAdapter.getmData();
-//      filter();
+    searchInput.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+     
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+          adapter.getFilter().filter(s);
+          search = s;
+      }
+
+      @Override
+      public void afterTextChanged(Editable s) {
+
+
+      }
+    });
+
       Log.d("checked","i am here");
-
-
-//    searchInput.addTextChangedListener(new TextWatcher() {
-//      @Override
-//      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//      }
-//
-//      @Override
-//      public void onTextChanged(CharSequence s, int start, int before, int count) {
-//
-//        search = s;
-////        Log.d("checked","search :"+search);
-////          setUpRecyclerView(search.toString());
-//
-//
-//      }
-//
-//      @Override
-//      public void afterTextChanged(Editable s) {
-//
-//      }
-//
-//
-//
-//    });
-
       floatingAddNewItemButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -146,66 +128,57 @@ public class MarketFragment extends Fragment {
 
         }
       });
-
-
     return contentView;
   }
 
-    private void setUpRecyclerView() {
-
-        Query query = productsOfMarketCollectionRef.orderBy("productTitle",Query.Direction.ASCENDING);
-         options = new FirestoreRecyclerOptions.Builder<productsListOfMarketFirestore>()
-                .setQuery(query,productsListOfMarketFirestore.class)
-                .build();
-        marketListAdapter = new ListAdapter_Market(options);
-        marketRecyleView.setHasFixedSize(true);
-        marketRecyleView.setLayoutManager(new LinearLayoutManager(this.getContext()));
-        marketRecyleView.setAdapter(marketListAdapter);
-
-    }
-
-
-//
-//    private void firebaseSearch(String searchText)
-//    {
-//      String search = searchText.toLowerCase();
-//      Query query = productsOfMarketCollectionRef.orderBy("title").startAt(search).endAt(search+"\uf8ff");
-//      options = new FirestoreRecyclerOptions.Builder<productsListOfMarketFirestore>()
-//              .setQuery(query,productsListOfMarketFirestore.class)
-//              .build();
-//
-//
-//      marketListAdapter = new FirebaseRecyclerAdapter<productsListOfMarketFirestore, ListAdapter_Market.ListViewHolder>() {
-//        @NonNull
-//        @Override
-//        public ListAdapter_Market.ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//
-//          View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.customlist_item_market,parent,false);
-//
-//
-//          ListAdapter_Market.ListViewHolder listViewHolder = new ListAdapter_Market.ListViewHolder(item);
-//          listViewHolder.
-//        }
-//
-//        @Override
-//        protected void onBindViewHolder(@NonNull ListAdapter_Market.ListViewHolder holder, int position, @NonNull productsListOfMarketFirestore model) {
-//
-//        }
-//      }
-
-
-//    }
 
 
     @Override
     public void onStart() {
         super.onStart();
-        marketListAdapter.startListening();
+
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        marketListAdapter.stopListening();
     }
+
+  private void getDataToArray() {
+    productsOfMarketCollectionRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+      @Override
+      public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+        if (queryDocumentSnapshots.isEmpty()) {
+          Log.d("checked", "database is empty");
+        } else {
+          List<productsListOfMarketFirestore> data = queryDocumentSnapshots.toObjects(productsListOfMarketFirestore.class);
+            adapter = new AlternateListAdapter_Market(mData,getContext());
+            marketRecyleView.setAdapter(adapter);
+          mData.addAll(data);
+          Log.d("checked success:", "ok " + mData);
+        }
+
+
+      }
+    }).addOnFailureListener(new OnFailureListener() {
+      @Override
+      public void onFailure(@NonNull Exception e) {
+        Log.d("checked", "data load failed");
+      }
+    });
+
+
+  }
+  private void setUpRecyclerViewManual() {
+    marketRecyleView.setHasFixedSize(true);
+    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+//    adapter = new AlternateListAdapter_Market(mData,this.getContext());
+//    marketRecyleView.setAdapter(adapter);
+    Log.d("checked","adapter called");
+
+    marketRecyleView.setLayoutManager(layoutManager);
+
+
+  }
+
 }
