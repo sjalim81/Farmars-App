d044a678f75c309bc96ff8bd22339d8e3f520966
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2020-08-10 15:24:55 +0600

    clone

--- app/src/main/java/com/example/farmersapp/util/MyAppGlideModule.java
+++ app/src/main/java/com/example/farmersapp/util/MyAppGlideModule.java
@@ -4,9 +4,12 @@ package com.example.farmersapp.util;
 import android.content.Context;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.BaseRequestOptions;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.storage.StorageReference;
 
@@ -21,5 +24,6 @@ public class MyAppGlideModule extends AppGlideModule {
         registry.append(StorageReference.class, InputStream.class,
                 new FirebaseImageLoader.Factory());
     }
+
 
 }
